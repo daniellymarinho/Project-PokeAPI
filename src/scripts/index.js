07d4815713e946a783a002837968cc6645d97a51
danielly marinho <danybrinquedo@hotmail.com>
2022-12-28 09:30:10 -0300

    entrega inicial

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,35 @@
-/* Monte sua lógica aqui */
+
+function render(array){
+    const list = document.querySelector("ul")
+    list.innerHTML = ''
+
+    array.forEach(Element => {
+        const card = createCard(Element)
+        list.appendChild(card)
+    })
+
+}
+
+function createCard(pokemon){
+    const li = document.createElement("li")
+    const div = document.createElement("div")
+    const img = document.createElement("img")
+    const nome = document.createElement("span")
+
+    const numeroNaPokedex = pokemon.url.slice(34, -1) // https://pokeapi.co/api/v2/pokemon/${payload.id}/
+      
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.substr(1)
+
+    img.src= `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${numeroNaPokedex}.png`;
+    nome.innerText = name
+
+    li.classList.add("pokemons")
+    div.classList.add("pokemon")
+    img.classList.add("image-pokemon")
+    nome.classList.add("name-pokemon")
+    
+    div.append(img, nome)
+    li.appendChild(div)
+
+    return li
+}
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,1 +1,39 @@
-/* Monte a lógica das requisições aqui */
+async function consumindoPokeAPI() {
+    const loading = document.querySelector("#loading")
+    const pokemonsAPI = await fetch("https://pokeapi.co/api/v2/pokemon")
+        .then(Response => Response.json())
+        .then(Response => {
+            render(Response.results)
+
+            return Response
+
+        })
+        .catch(error => console.log(error))
+    loading.classList.add("hidden")
+
+    return pokemonsAPI
+
+}
+
+async function getPokemonByName(pokemonName) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    const response = await fetch(url)
+    const payload = await response.json()
+    render([{ ...payload, url: `https://pokeapi.co/api/v2/pokemon/${payload.id}/` }])
+}
+
+function renderizaSearch() {
+    const input = document.querySelector("input")
+    const button = document.querySelector("#button-search")
+
+    button.addEventListener("click", () => {
+        getPokemonByName(input.value.toLowerCase().trim())
+    })
+    input.addEventListener('change', ()=>{
+        if (input.value === '')
+        {consumindoPokeAPI()}
+    })
+}
+
+renderizaSearch()
+consumindoPokeAPI()
